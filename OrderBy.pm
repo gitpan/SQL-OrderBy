aa78c041c5acd43cc14cdd86e29a7c3550d4672a
Gene Boggs <gene@cpan.org>
2003-02-25 23:36:16 +0000

    Import of GENE/SQL-OrderBy-0.07 from CPAN.
    gitpan-cpan-distribution: SQL-OrderBy
    gitpan-cpan-version:      0.07
    gitpan-cpan-path:         GENE/SQL-OrderBy-0.07.tar.gz
    gitpan-cpan-author:       GENE
    gitpan-cpan-maturity:     released

--- OrderBy.pm
+++ OrderBy.pm
@@ -1,57 +1,46 @@
 package SQL::OrderBy;
 
 use strict;
-# Use Exporter for a single function? Nah.
-#use base 'Exporter';
-#use vars qw(@EXPORT @EXPORT_OK);
-#@EXPORT_OK = @EXPORT = qw(
-#    toggle_resort
-#);
+use base 'Exporter';
+use vars qw(@EXPORT @EXPORT_OK);
+@EXPORT_OK = @EXPORT = qw(
+    toggle_resort
+    get_columns
+    col_dir_list
+    num2asc_desc
+);
 use vars qw($VERSION);
-$VERSION = '0.06.2';
+$VERSION = '0.07';
 
+# sub toggle_resort {{{
 # Transform an order by clause.
 sub toggle_resort {
     my %args = @_;
+    _set_defaults(\%args);
 
     # Declare an ordered array and a direction hash of columns.
-    my ($columns, %columns);
+    my ($columns, $direction);
 
-    # Set the column list and the order directions.
-    if (ref $args{order_by} eq 'ARRAY') {
-        # The order clause was sent in as a list.
-        $columns = $args{order_by};
-
-        # Set the column direction hash.
-        for (@$columns) {
-            if (/^(.*?)(?:\s+(asc|desc))?$/i) {
-                # Use the direction provided; Ascend by default.
-                $columns{$1} = $2 ? $2 : 'asc';
-            }
-        }
-    }
-    else {
-        # The order clause was sent in as a string.
-        # Set the column direction hash.
-        for (split /\s*,\s*/, $args{order_by}) {
-            if (/^(.*?)(?:\s+(asc|desc))?$/i) {
-                # Use the direction provided; Ascend by default.
-                $columns{$1} = $2 ? $2 : 'asc';
-            }
-
-            # Add the column to our columns array.
-            push @$columns, $1;
-        }
-    }
+    # Set the column name list and the directions.
+    ($columns, $direction) = get_columns(
+        order_by => $args{order_by},
+        show_ascending => $args{show_ascending},
+        name_direction => 1,
+        numeric_direction => 1,
+    );
 
     # Handle a selected column.
     if (my $selected = $args{selected}) {
-        # Toggle if the selected column is already the first one.
-        $columns{$selected} =
-            $selected eq $columns->[0] &&
-            exists $columns{$selected} &&
-            $columns{$selected} eq 'asc'
-            ? 'desc' : 'asc';
+        # Set the direction according to our show_ascending flag.
+        $direction->{$selected} =
+            !$direction->{$selected} || $direction->{$selected}
+                ? 1 : 0;
+
+        # Toggle if the selected column is the first one.
+        if ($selected eq $columns->[0]) {
+            $direction->{$selected} = $direction->{$selected}
+                ? 0 : 1;
+        }
 
         # Remove the selected column name from its old position.
         @$columns = grep { $_ ne $selected } @$columns;
@@ -59,12 +48,96 @@ sub toggle_resort {
         unshift @$columns, $selected;
     }
 
+    %$direction = num2asc_desc ($direction, $args{show_ascending})
+        unless $args{numeric_direction};
+    
+    @$columns = col_dir_list ($columns, $direction);
+
     # Return the column ordering as an arrayref or string.
-    return wantarray
-        ? map { "$_ $columns{$_}" } @$columns
-        : join ', ', map { "$_ $columns{$_}" } @$columns;
-#        return join ', ', map { "$_ $columns{$_}" } @$columns;
+    return wantarray ? @$columns : join ', ', @$columns;
 }
+# }}}
+
+# sub get_columns {{{
+# Return the column names and directions as either hash/array
+# references, or a column array, or an "order by" clause.
+sub get_columns {
+    my %args = @_;
+    _set_defaults(\%args);
+
+    # Bail out unless we are given an order by clause or full SQL.
+    die "No statement or clause provided.\n" unless $args{order_by};
+
+    # Strip off any unneeded SQL clauses.
+    $args{order_by} =~ s/^.*?\border by\s+(.*)$/$1/i;
+
+    # Hold column names and directions.
+    my ($columns, $direction);
+
+    # The ordering was sent in as either a list or a clause.
+    my @order = ref $args{order_by} eq 'ARRAY'
+        ? @{ $args{order_by} }
+        : split /\s*,\s*/, $args{order_by};
+
+    # Set the column array and direction hash.
+    for (@order) {
+        if (/^(.*?)(?:\s+(asc|desc))?$/i) {
+            # Use the direction provided; Ascend by default.
+            $direction->{$1} = $2 && $2 eq 'desc' ? 0 : 1;
+        }
+
+        # Add the column to our columns array.
+        push @$columns, $1;
+    }
+
+    %$direction = num2asc_desc ($direction, $args{show_ascending})
+        unless $args{numeric_direction};
+
+    # NOTE: name_direction only makes sense in an array context.
+    if ($args{name_direction}) {
+        $columns = [ $columns, $direction ];
+    }
+    else {
+        @$columns = col_dir_list ($columns, $direction);
+    }
+
+    return wantarray ? @$columns : join ', ', @$columns;
+}
+# }}}
+
+# sub col_dir_list {{{
+# Return an array of column names with their respective directions concatinated.
+sub col_dir_list {
+    my ($columns, $direction) = @_;
+    return map {
+        $direction->{$_}
+            ? "$_ $direction->{$_}"
+            : $_
+    } @$columns;
+}
+# }}}
+
+# sub num2asc_desc {{{
+# Return directions as "asc" and "desc" in place of their numeric eqivalents.
+sub num2asc_desc {
+    my ($dir, $show_ascending) = @_;
+    return map {
+        $_ => $dir->{$_}
+            ? $show_ascending ? 'asc' : ''
+            : 'desc'
+    } keys %$dir;
+}
+# }}}
+
+# sub _set_defaults {{{
+# Naive little default argument setter.
+sub _set_defaults {
+    my $args = shift;
+    $args->{show_ascending}    = 1 unless defined $args->{show_ascending};
+    $args->{name_direction}    = 0 unless defined $args->{name_direction};
+    $args->{numeric_direction} = 0 unless defined $args->{numeric_direction};
+}
+# }}}
 
 1;
 __END__
@@ -77,25 +150,51 @@ SQL::OrderBy - Transform an SQL ORDER BY clause.
 
   use SQL::OrderBy;
 
-  # Array context
-  my @order = SQL::OrderBy::toggle_resort(
+  # Fetch the columns in array context.
+  @columns = get_columns (
+      order_by => 'name, artist desc, album',
+  );
+  # ('name asc', 'artist desc', 'album asc')
+
+  # Fetch the columns in scalar context without the asc keyword.
+  $columns = get_columns (
+      order_by => ['name', 'artist desc', 'album'],
+      show_ascending => 0,
+  );
+  # 'name, artist desc, album'
+
+  # Fetch the columns as a name array and numeric direction hash.
+  @columns = get_columns (
+      order_by => 'name, artist desc, album',
+      name_direction => 1,
+      numeric_direction => 1,
+  );
+  # (['name','artist','album'], {name=>1, artist=>0, album=>1})
+
+  @columns = col_dir_list (\@column_names, \%directions);
+
+  %directions = num2asc_desc (\%directions, 0);
+
+  # Toggle resort in array context.
+  @order = toggle_resort (
       selected => 'artist',
-      order_by => [ qw(name artist album) ],
+      order_by => ['name', 'artist', 'album'],
   );
   # ('artist asc', 'name asc', 'album asc')
 
-  # Scalar context
-  print scalar SQL::OrderBy::toggle_resort(
+  # Toggle resort in scalar context without the asc keyword.
+  print scalar toggle_resort (
+      show_ascending => 0,
       selected => 'time',
-      order_by => scalar SQL::OrderBy::toggle_resort(
+      order_by => scalar toggle_resort(
           selected => 'artist',
-          order_by => scalar SQL::OrderBy::toggle_resort(
+          order_by => scalar toggle_resort(
               selected => 'artist',
-              order_by => 'name, artist, album'
+              order_by => 'name asc, artist asc, album asc'
           )
       )
   );
-  # 'time asc, artist desc, name asc, album asc'
+  # 'time, artist desc, name, album'
 
 =head1 ABSTRACT
 
@@ -112,7 +211,7 @@ integrity checking is done.
 
 =head1 FUNCTIONS
 
-=head2 toggle_resort()
+=head2 toggle_resort ()
 
   toggle_resort(
       order_by => $order_clause_or_list,
@@ -135,6 +234,18 @@ In a scalar context, this function returns the clause as a (CSV)
 string.  In an array context, this function returns a list of column
 names with their respective sort directions.
 
+This function optionally takes Boolean flags affecting the returned
+data structure.  These are:
+ 
+show_ascending => Expose the asc column directions.  Defaults on (1).
+
+name_direction => Return references to the column names and their
+directions.  Defaults off (0).  Only makes sense in array context.
+
+numeric_direction => Return Boolean column directions, instead of
+asc/desc.  Defaults off (0).  Only makes sense with the
+name_direction flag on.
+
 This implements an essential feature for GUI environments, where the
 user interacts with a table by sorting and resorting with a mouse and
 "toggle button column headings" during an interactive search
@@ -146,15 +257,70 @@ is, no "toggling" or moving is done.
 
 * Currently, this function is not exported by default.
 
+=head2 get_columns ()
+
+  @columns = get_columns (
+      order_by => $order_clause_or_list,
+      show_ascending    => $x,
+      name_direction    => $y,
+      numeric_direction => $z,
+  )
+
+  $columns = get_columns (
+      order_by => $order_clause_or_list,
+      show_ascending => $x,
+  )
+
+This function simply returns a well formed order by clause or list.
+It can accept either a string or array reference for the order_by
+argument.
+
+In a scalar context, this function returns the clause as a (CSV)
+string.  In an array context, this function returns a list of column
+names with their respective sort directions.
+
+This function optionally takes Boolean flags affecting the returned
+data structure.  These are:
+ 
+show_ascending => Expose the asc column directions.  Defaults on (1).
+
+name_direction => Return references to the column names and their
+directions.  Defaults off (0).  Only makes sense in array context.
+
+numeric_direction => Return Boolean column directions, instead of
+asc/desc.  Defaults off (0).  Only makes sense with the
+name_direction flag on.
+
+=head2 col_dir_list ()
+
+  @columns = col_dir_list (\@columns, \%asc_desc);
+
+Return an array of column names with their respective directions
+concatinated.
+
+This function takes a reference to an array of column names and a
+reference to a direction hash.
+
+=head2 num2asc_desc ()
+
+  %directions = num2asc_desc (\%directions, $show_asc)
+
+Return directions as "asc" and "desc" in place of their numeric
+eqivalents.
+
+This function takes a reference to a direction hash and an optional
+flag to control the display of the asc keyword.
+
 =head1 DEPENDENCIES
 
 None.
 
 =head1 TODO
 
-Add functions for different kinds of resorting.
+Add functions for different kinds of resorting?
 
-Add functions for handling different module statement objects.
+Add the ability to return the order by clause without altering the
+case (or display) of column names and directions.
 
 =head1 HISTORY
 
--- t/1.t
+++ t/1.t
@@ -1,51 +1,141 @@
 use strict;
-use Test::More tests => 6;
+use Test::More tests => 17;
 
 BEGIN { use_ok('SQL::OrderBy') };
 
-# Set context.
-my $clause_string = 'name, artist, album';
-my $column = 'artist';
-my $result = 'artist asc, name asc, album asc';
-
-# Scalar context
-my $order = SQL::OrderBy::toggle_resort(
-    selected => $column,
-    order_by => $clause_string,
+# fetch a numeric name_direction list
+my @columns = SQL::OrderBy::get_columns(
+    order_by => 'name, artist desc, album',
+    show_ascending => 1,
+    name_direction => 1,
+    numeric_direction => 1,
 );
-is $order, $result,
+is join (', ', @{ $columns[0] }),
+    'name, artist, album',
+    'column name list for name_direction';
+is join (', ', map { "$_ $columns[1]->{$_}" } sort keys %{ $columns[1] }),
+    'album 1, artist 0, name 1',
+    'column directions for numeric name_direction';
+
+# fetch a asc/desc name_direction list
+@columns = SQL::OrderBy::get_columns(
+    order_by => 'name, artist desc, album',
+    show_ascending => 1,
+    name_direction => 1,
+    numeric_direction => 0,
+);
+is join (', ', map { "$_ $columns[1]->{$_}" } sort keys %{ $columns[1] }),
+    'album asc, artist desc, name asc',
+    'column directions for asc/desc name_direction';
+
+# convert column directions
+my %direction = (name => 1, artist => 0, album => 1);
+%direction = num2asc_desc (\%direction, 0);
+is join (', ', map { $direction{$_} ? "$_ $direction{$_}" : $_ } sort keys %direction),
+    'album, artist desc, name',
+    'numeric column directions to hidden asc/desc';
+%direction = (name => 1, artist => 0, album => 1);
+%direction = num2asc_desc (\%direction, 1);
+is join (', ', map { $direction{$_} ? "$_ $direction{$_}" : $_ } sort keys %direction),
+    'album asc, artist desc, name asc',
+    'numeric column directions to exposed asc/desc';
+
+# render a column name direction list
+%direction = (name => 'asc', artist => 'desc', album => 'asc');
+@columns = col_dir_list ([qw(name artist album)], \%direction);
+is join (', ', @columns), 'name asc, artist desc, album asc',
+    'column name direction list rendered';
+
+# fetch column names with exposed direction
+# in array context
+@columns = SQL::OrderBy::get_columns(
+    order_by => 'name, artist desc, album',
+    show_ascending => 1,
+    name_direction => 0,
+);
+is join (', ', @columns), 'name asc, artist desc, album asc',
+    'column names with exposed direction in array context';
+# in scalar context
+my $columns = SQL::OrderBy::get_columns(
+    order_by => ['name', 'artist desc', 'album'],
+    show_ascending => 1,
+    name_direction => 0,
+);
+is $columns, 'name asc, artist desc, album asc',
+    'column names with exposed direction in scalar context';
+
+# fetch column names with hidden asc
+# in array context
+@columns = SQL::OrderBy::get_columns(
+    order_by => 'name asc, artist desc, album',
+    show_ascending => 0,
+    name_direction => 0,
+);
+is join (', ', @columns), 'name, artist desc, album',
+    'column names with hidden asc in array context';
+# in scalar context
+$columns = SQL::OrderBy::get_columns(
+    order_by => ['name', 'artist desc', 'album'],
+    show_ascending => 0,
+    name_direction => 0,
+);
+is $columns, 'name, artist desc, album',
+    'column names with hidden asc in scalar context';
+
+# toggle in scalar context
+my $order = SQL::OrderBy::toggle_resort(
+    selected => 'artist',
+    order_by => 'name, artist, album',
+);
+is $order, 'artist asc, name asc, album asc',
     'order clause in scalar context';
 $order = SQL::OrderBy::toggle_resort(
-    selected => $column,
+    selected => 'artist',
     order_by => [ qw(name artist album) ],
 );
-is $order, $result,
+is $order, 'artist asc, name asc, album asc',
     'order array in scalar context';
 
-# Array context
+# toggle in array context
 my @order = SQL::OrderBy::toggle_resort(
-    selected => $column,
-    order_by => $clause_string,
+    selected => 'artist',
+    order_by => 'name, artist, album',
 );
-is join (', ', @order), $result,
+is join (', ', @order), 'artist asc, name asc, album asc',
     'order clause in array context';
 @order = SQL::OrderBy::toggle_resort(
-    selected => $column,
+    selected => 'artist',
     order_by => [ qw(name artist album) ],
 );
-is join (', ', @order), $result,
+is join (', ', @order), 'artist asc, name asc, album asc',
     'order array in array context';
 
-# Nested transformation
+# hidden asc nested toggle
 $order = SQL::OrderBy::toggle_resort(
+    show_ascending => 0,
     selected => 'time',
     order_by => scalar SQL::OrderBy::toggle_resort(
-        selected => $column,
+        selected => 'artist',
         order_by => scalar SQL::OrderBy::toggle_resort(
-            selected => $column,
-            order_by => $clause_string
+            selected => 'artist',
+            order_by => 'name asc, artist asc, album asc',
+        )
+    )
+);
+is $order, 'time, artist desc, name, album',
+    'hidden asc nested transformation';
+
+# exposed asc nested toggle
+$order = SQL::OrderBy::toggle_resort(
+    show_ascending => 1,
+    selected => 'time',
+    order_by => scalar SQL::OrderBy::toggle_resort(
+        selected => 'artist',
+        order_by => scalar SQL::OrderBy::toggle_resort(
+            selected => 'artist',
+            order_by => 'name, artist, album',
         )
     )
 );
 is $order, 'time asc, artist desc, name asc, album asc',
-    'nested transformation';
+    'exposed asc nested transformation';
